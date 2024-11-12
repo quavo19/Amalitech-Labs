fc943633b4c887f4eb99f39ed43fe37d16928d72
quavo19 <donald.akite@amalitech.com>
2024-11-12 16:17:28 +0000

    Update changes

--- Week1/Challenge1/index.js
+++ Week1/Challenge1/index.js
@@ -1,14 +1,18 @@
 // Capitalize first letter of a string
-const CapitalizeStr = (str) => {
-    return (str.charAt(0).toUpperCase() + str.slice(1))
-}
+const CapitalizeStr = (str) => str.charAt(0).toUpperCase() + str.slice(1)
+
 //console.log(CapitalizeStr("donald"), "capitalized")
 
 
 
 // Reverse a string
 const ReverseStr = (str) => {
-    return str.split("").reverse().join("")
+   // return str.split("").reverse().join("")
+   let name = ""
+    for(i=str.length - 1; i >= 0; i--){
+        name+=str[i]
+    }
+    return name;
 }
 //console.log(ReverseStr("donald"), "reversed")
 
@@ -26,13 +30,16 @@ const IsPalindrome = (str) => {
 
 // count the number of words in a string
 const WordCount = (str) => {
-    return str.split("").length
+   //return (str.match(/\b\w+\b/g)).length;
+   return str.split(" ").length
+
+   //return str.match(/\S+/g).length
 }
-// console.log(WordCount("donald"))
+ //console.log(WordCount("Regular expressions are patterns used to match character combinations in strings. In JavaScript, regular expressions are also objects. These patterns are used with the"))
 
 
 
-//double every number in a array
+//double every number in an array
 const DoubleArr = (arr) => {
     console.log(arr)
     return (arr.length > 0 ? arr.map((number) => number*2) : "array is empty")
@@ -42,14 +49,14 @@ const testArr = [2,3,5,7,2,8,3,5]
 
 
 
-//filter out even numbers 
+//filter out even numbers using a for loop
 const EvenNumbersOnly = (arr) => {
     return arr.filter((number) => number %2 === 0)
 }
 //console.log(EvenNumbersOnly(testArr))
 
 
-//Calculate the sum of all numbers in an array
+//Calculate the sum of all numbers in an array using a for loop
 const SumAllNumbers = (arr) => {
     return arr.reduce((acc, inc) => acc + inc)
 }
@@ -59,7 +66,7 @@ const SumAllNumbers = (arr) => {
 
 //Calculate the average of all numbers
 const AverageNumber = (arr) => {
-    return (arr.length / arr.reduce((a,b)=> a+b)).toFixed(2)
+    return ( arr.reduce((a,b)=> a+b) / arr.length).toFixed(2)
 }
 //console.log(AverageNumber(testArr))
 
@@ -86,10 +93,10 @@ const IsAdult = (person) => {
 
 
 
-//Filter an array of objects to keep those at least minAge years old
+//Filter an array of objects to keep those at least minAge years old using for loop
 const FilterByAge = (people, minAge) => {
 return (
-    ((typeof minAge === "number") ? (people.filter((person) => person.age > minAge)) : `"${minAge}" is not a number, enter a number`)
+    ((typeof minAge === "number") ? (people.filter((person) => person.age >= minAge)) : `"${minAge}" is not a number, enter a number`)
 )}
 
 let people = [
@@ -101,11 +108,11 @@ let people = [
     {name: "Eddie", age: 12},
     {name: "Karl", age: 22},
 ]
-// console.log(FilterByAge(people, "two"))
+console.log(FilterByAge(people, 12))
 
 
 
-//Function Composition
+//Function Composition 
 const compose = (...functions) => {
     return (input) => {
       return functions.reduceRight((acc, fn) => {
@@ -115,4 +122,4 @@ const compose = (...functions) => {
   };
 const Composition = compose(DoubleArr,EvenNumbersOnly)
 
-console.log(Composition([32,34,1,2,4,34,6,2,3,12,34,4]))
+// console.log(Composition([32,34,1,2,4,34,6,2,3,12,34,4]))
